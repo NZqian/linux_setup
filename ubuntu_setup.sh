9dd2f5db0ab953a66394da481fe9d96bfa797acf
ning <asdf@asdf.com>
2020-12-08 14:14:00 +0800

    ros

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 sudo add-apt-repository ppa:dawidd0811/neofetch
 sudo add-apt-repository ppa:kgilmer/speed-ricer
-sudo apt-get install git zsh curl wget axel neofetch i3-gaps feh picom 
+sudo add-apt-repository ppa:mmstick76/alacritty
+sudo apt-get install git zsh curl wget axel neofetch i3-gaps feh picom alacritty

9c8d686545e6eb79685e1c0a14a90be10c2767b0
ning <asdf@asdf.com>
2020-12-17 11:54:13 +0800

    .

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -3,3 +3,4 @@ sudo add-apt-repository ppa:dawidd0811/neofetch
 sudo add-apt-repository ppa:kgilmer/speed-ricer
 sudo add-apt-repository ppa:mmstick76/alacritty
 sudo apt-get install git zsh curl wget axel neofetch i3-gaps feh picom alacritty
+sudo apt-get install python3-psutil
